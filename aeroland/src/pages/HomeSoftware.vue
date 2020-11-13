de13de8518ff9555cff52fcf8a1152109c78d4bf
Michael Morehead <mmorehea@mix.wvu.edu>
2020-11-13 11:43:39 -0500

    Added gallery for syGlass View projects

--- aeroland/src/pages/HomeSoftware.vue
+++ aeroland/src/pages/HomeSoftware.vue
@@ -13,14 +13,14 @@
             <div class="container">
                 <div class="section-title-wrapper text-center section-space--mb_100">
                     <h2 class="section-title font-weight--light mb-30 wow move-up">Your data as a hologram... grab it!</h2>
-					<font size="+2"> syGlass is a software product that allows you to render large, volumetric data in its true, three-or-four dimensional form. Modern imaging techniques leave you inundated with large volumetric data, but syGlass can prepare up to 20 TB of data at a time, without crashing.
+					<p> syGlass is a software product that allows you to render large, volumetric data in its true, three-or-four dimensional form. Modern imaging techniques leave you inundated with large volumetric data, but syGlass can prepare up to 20 TB of data at a time, without crashing.
 
 Data is scaled to allow you to see minute details in extreme detail. Perform analysis with enhanced precision, then annotate the data in syGlass with a vast array of practical tools. 
     
 syGlass has a mission to help scientists, researchers, and educators like you reach your full potential by providing the tools and technology you need to explore, understand, and communicate your data.
 
 
-</font size="+2">
+</p>
 
                     <div class="video-button-container video-popup">
                         <silentbox-single src="https://youtu.be/ieCdFXgDXZ8">
@@ -111,6 +111,8 @@ syGlass has a mission to help scientists, researchers, and educators like you re
 
 		<!-- call to action component -->
         <CtaThree />
+		
+		<PortfolioGridCaption />
 
         <!-- accordion section -->
         <div class="software-accordion-wrapper section-space--pt_120 section-space--pb_60 gray-gradient" id="faq">
@@ -168,6 +170,8 @@ syGlass has a mission to help scientists, researchers, and educators like you re
 
         <!-- offcanvas search component -->
         <OffcanvasSearchBox />
+		
+		
 
 	</div>
 </template>
@@ -175,38 +179,32 @@ syGlass has a mission to help scientists, researchers, and educators like you re
 <script>
 	import HeaderSoftware from "../components/HeaderSoftware";
 	import HeroSoftware from "../components/HeroSoftware";
-	import BrandLogoCarousel from "../components/BrandLogoCarousel";
 	import FeatureList from "../components/FeatureList";
 	import TabTwo from "../components/TabTwo";
 	import ProgressBar from "../components/ProgressBar";
-	import PricingOne from "../components/PricingOne";
 	import CurveShape from "../components/CurveShape";
 	import CtaThree from "../components/CtaThree";
 	import FunFactOne from "../components/FunFactOne";
 	import Accordion from "../components/Accordion";
-	import Newsletter from "../components/Newsletter";
 	import FooterMain from "../components/FooterMain";
 	import OffcanvasSearchBox from "../components/OffcanvasSearchBox";
-	import StartUpFeature from "../components/StartUpFeature";
+	import PortfolioGridCaption from "../pages/portfolio/PortfolioGridCaption";
 
 	export default {
 		name: "app",
 		components: {
 			HeaderSoftware,
 			HeroSoftware,
-			StartUpFeature,
-            BrandLogoCarousel,
             FeatureList,
             TabTwo,
             ProgressBar,
-            PricingOne,
 			CurveShape,
 			CtaThree,
             FunFactOne,
             Accordion,
-            Newsletter,
             FooterMain,
-            OffcanvasSearchBox	
+            OffcanvasSearchBox	,
+			PortfolioGridCaption
 		}
 	};
 </script>
