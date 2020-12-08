78c60d19a6651ad74fca0424c2a239707f8018f1
Michael Morehead <mmorehea@mix.wvu.edu>
2020-12-08 09:46:06 -0500

    added some text

--- aeroland/src/components/CtaThree.vue
+++ aeroland/src/components/CtaThree.vue
@@ -8,7 +8,7 @@
                         <div class="row align-items-center">
                             <div class="col-lg-6 order-2 order-lg-1">
                                 <div class="cta-image__content">
-                                    <h4 class="cta-image__title text-white mb-20 wow move-up">Free download of syGlass View now</h4>
+                                    <h4 class="cta-image__title text-white mb-20 wow move-up">Free download of syGlass View</h4>
                                     <div class="cta-image__text text-white mb-20 wow move-up">Offer your data with the best assistance for understanding.</div>
                                     <router-link to="" class="btn btn--green wow move-up">
                                         <span class="btn-icon  ml-0 mr-2"><i class="fa fa-arrow-alt-to-bottom"></i></span> Get syGlass
--- aeroland/src/pages/portfolio/PortfolioGridCaption.vue
+++ aeroland/src/pages/portfolio/PortfolioGridCaption.vue
@@ -7,23 +7,24 @@
         <div class="portfolio-pages-wrapper section-space--ptb_100 border-bottom gray-gradient">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-3 col-md-3 mb-30 wow move-up" v-for="portfolio in portfolios" :key="portfolio.id">
-                        <div class="portfolio-grid-caption">
-                            <router-link to="/portfolio-details" class="single-grid-caption">
-                                <div class="single-portfolio__thumbnail">
-                                    <img class="img-fluid" :src="portfolio.thumbnailUrl" :alt="portfolio.name">
-                                </div>
-                            </router-link>
-                            <div class="post-info">
-                                <div class="post-categories">
-                                    <router-link to="">{{ portfolio.name }}</router-link>
-                                </div>
-                                <h5 class="post-title font-weight--bold">
-                                    <router-link to="/portfolio-details">{{ portfolio.name }}</router-link>
-                                </h5>
-                            </div>
-                        </div>
-                    </div>
+					<div class="column">
+						<div class="col-lg-3 col-md-3 mb-30 wow move-up" v-for="portfolio in portfolios" :key="portfolio.id">
+							<div class="portfolio-grid-caption">
+								<router-link to="/portfolio-details" class="single-grid-caption">
+									<div class="single-portfolio__thumbnail">
+										<img class="img-fluid" :src="portfolio.thumbnailUrl" :alt="portfolio.name">
+									</div>
+								</router-link>
+								<div class="post-info">
+									<div class="post-categories">
+										<router-link to="">{{ portfolio.modality }}</router-link>
+									</div>
+									<h5 class="post-title font-weight--bold">
+										<router-link to="/portfolio-details">{{ portfolio.name }}</router-link>
+									</h5>
+								</div>
+							</div>
+						</div>
                     
                     <div class="col-lg-12">
                         <div class="load-more-button text-center">
